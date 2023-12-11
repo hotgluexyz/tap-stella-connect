6f9419a5b8f46f4202da5649c3ee3da26c0655d2
Vinicius Mesel <bioinfo.vinicius@gmail.com>
2023-12-11 11:51:41 -0300

    Adds replication key to stream and get_starting_time logic (#7)

--- tap_stella_connect/client.py
+++ tap_stella_connect/client.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 import sys
 import jwt
-from datetime import datetime
+import requests
+
+from pendulum import parse
+from datetime import datetime, timedelta
 from typing import Any, Iterable
 
-import requests
 from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams import RESTStream
 
@@ -25,16 +27,16 @@ class TapStellaConnectStream(RESTStream):
         """Return the API URL root, configurable via tap settings."""
         return "https://api.stellaconnect.net/v2"
 
-    records_jsonpath = "$[*]"  
+    records_jsonpath = "$[*]"
 
     def get_next_page_token(
         self, response: requests.Response, previous_token: Optional[Any]
     ) -> Any:
-       
+
         next_page_token = None
         data = response.json()
-        if len(data)>0:
-            #Pagination is calculated by sequence_id
+        if len(data) > 0:
+            # Pagination is calculated by sequence_id
             next_page_token = data[-1].get("sequence_id")
 
         return next_page_token
@@ -43,29 +45,33 @@ class TapStellaConnectStream(RESTStream):
     def http_headers(self) -> dict:
         headers = {}
 
-        #make the JWT for process
-        # timestamp = datetime.now()
-        # iat = datetime.fromtimestamp(timestamp, timezone.utc)
-        jwt_to_use = jwt.encode({"iss": "Hotglue Tap", "iat": datetime.now()}, self.config['api_secret'], algorithm="HS256")
+        jwt_to_use = jwt.encode({
+            "iss": "Hotglue Tap",
+            "iat": datetime.now()
+        }, self.config['api_secret'], algorithm="HS256")
 
         headers["Authorization"] = jwt_to_use
         headers["x-api-key"] = self.config["api_key"]
-        
-        if "user_agent" in self.config:
-            headers["User-Agent"] = self.config.get("user_agent")
-        else:
-            headers["User-Agent"] = "hotglue_dummy_agent"
+        headers["User-Agent"] = self.config.get("user_agent", "hotglue_dummy_agent")
         return headers
-    
+
+    def get_starting_time(self, context):
+        start_date = self.config.get("start_date")
+        if start_date:
+            start_date = parse(self.config.get("start_date"))
+        rep_key = self.get_starting_timestamp(context) + timedelta(seconds=1)
+        return rep_key or start_date
+
+
     def get_url_params(
         self,
-        context: dict | None,  # noqa: ARG002
-        next_page_token: Any | None,  # noqa: ANN401
+        context: dict | None,
+        next_page_token: Any | None,
     ):
-
         params: dict = {}
         if self.replication_key:
-            params['created_at_gte'] = self.config['start_date']
+            params['created_at_gte'] = self.get_starting_time(context)
+
         params["limit"] = self._page_size
         if next_page_token:
             params['after'] = next_page_token
